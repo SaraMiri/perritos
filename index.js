c289031ed174d208f2686ec24dea70400b0b765a
Sara Miragaya Rico <saramiragaya@gmail.com>
2018-10-30 13:49:49 +0100

    Colocada variable dentro da función e () que faltaba

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const RANDOM_DOG_URL = "https://dog.ceo/api/breeds/image/random";
-
 document.querySelector(".add-doggo").addEventListener("click", onClick);
 
 function onClick(x) {
+  const RANDOM_DOG_URL = "https://dog.ceo/api/breeds/image/random";
   fetch(RANDOM_DOG_URL)
     .then(function(apiResponse) {
-      return apiResponse.json;
+      return apiResponse.json();
     })
     .then(function(jsonResponse) {
       const img = document.createElement("img");
