60ee0fa7fbb25350697d4b3846b30d4d987b4cf5
Sara Miragaya Rico <saramiragaya@gmail.com>
2018-10-31 08:47:46 +0100

    Añadida function appendingToDom

--- index.js
+++ index.js
@@ -6,17 +6,23 @@ function start() {
 
 function onClick(x) {
   const RANDOM_DOG_URL = "https://dog.ceo/api/breeds/image/random";
+
   fetch(RANDOM_DOG_URL)
     .then(toJSON)
-    .then(function(jsonResponse) {
-      const img = document.createElement("img");
-      img.alt = "Perrito bonito";
-      img.src = jsonResponse.message;
-
-      document.querySelector(".doggos").appendChild(img);
-    });
+    .then(appendImgToDOM);
 }
-
 function toJSON(apiResponse) {
   return apiResponse.json();
+}
+
+function appendImgToDOM(jsonResponse) {
+  const img = createImage(jsonResponse.message);
+  document.querySelector(".doggos").appendChild(img);
+}
+
+function createImage(dogUrl) {
+  const img = document.createElement("img");
+  img.alt = "Perrito bonito";
+  img.src = dogUrl;
+  return img;
 }
